2e1db2c5584e06775b98c7cec51306b0392b23d2
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2026-01-07 14:35:57 +0700

    2

--- resources/views/frontend_dashboard_add_listing.blade.php
+++ resources/views/frontend_dashboard_add_listing.blade.php
@@ -1,11 +1,19 @@
 @extends('frontends.master')
 
+@section('hide_newsletter')@endsection
+@section('hide_footer')@endsection
+
 @section('title', 'Đăng tin mới - Đà Lạt BDS')
 
 @section('content')
-    {{-- @include('frontends.components.dashboard_add_listing') --}}
+    @include('frontends.components.dashboard_add_listing')
 @endsection
 
 @push('styles')
-    
+    <link rel="stylesheet" href="{{ asset('css/dashboard-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/webapp.css') }}">
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('js/dashboard.js') }}"></script>
 @endpush
--- resources/views/frontend_dashboard_reviews.blade.php
+++ resources/views/frontend_dashboard_reviews.blade.php
@@ -1,11 +1,18 @@
 @extends('frontends.master')
+@section('hide_newsletter')@endsection
+@section('hide_footer')@endsection
 
 @section('title', 'Đánh giá - Đà Lạt BDS')
 
 @section('content')
-    {{-- @include('frontends.components.dashboard_reviews') --}}
+    @include('frontends.components.dashboard_reviews')
 @endsection
 
 @push('styles')
-   
+    <link rel="stylesheet" href="{{ asset('css/dashboard-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/webapp.css') }}">
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('js/dashboard.js') }}"></script>
 @endpush
