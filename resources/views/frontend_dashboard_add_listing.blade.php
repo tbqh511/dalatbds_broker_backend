50725dd70642b691b9e93d2ec4f7dd1593e4b7f4
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2026-01-21 13:02:39 +0700

    revert to map

--- resources/views/frontend_dashboard_add_listing.blade.php
+++ resources/views/frontend_dashboard_add_listing.blade.php
@@ -166,7 +166,7 @@
                 },
 
                 // Initialize Google Map inside picker
-                async initGoogleMap() {
+                initGoogleMap() {
                     console.log("Start initGoogleMap");
                     const defaultPos = { lat: 11.940419, lng: 108.458313 };
 
@@ -175,49 +175,51 @@
                         return;
                     }
 
-                    // Import AdvancedMarkerElement
-                    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
                     this.pickerMap = new google.maps.Map(document.getElementById("picker-map"), {
                         center: defaultPos,
                         zoom: 15,
-                        mapId: "DEMO_MAP_ID",
                         disableDefaultUI: true,
                         clickableIcons: false,
                         gestureHandling: "greedy",
                     });
 
-                    console.log("Map instance created", this.pickerMap);
-
-                    // Add AdvancedMarker
-                    this.pickerMarker = new AdvancedMarkerElement({
-                        map: this.pickerMap,
+                    this.pickerMarker = new google.maps.Marker({
                         position: defaultPos,
-                        gmpDraggable: true,
-                        title: "Kéo thả để chọn vị trí"
+                        map: this.pickerMap,
+                        draggable: true,
+                        animation: google.maps.Animation.DROP,
+                        icon: {
+                            url: "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi2.png",
+                            scaledSize: new google.maps.Size(27, 43)
+                        }
                     });
 
+                    console.log("Map instance created", this.pickerMap);
                     console.log("Marker created", this.pickerMarker);
 
                     this.pickerGeocoder = new google.maps.Geocoder();
 
-                    // Marker drag listener
+                    // Sự kiện drag cho marker cũ
                     this.pickerMarker.addListener("dragend", (event) => {
-                        const position = this.pickerMarker.position;
-                        if (position.lat && typeof position.lat === 'function') {
-                             this.pickerLat = position.lat();
-                             this.pickerLng = position.lng();
-                        } else {
-                             this.pickerLat = position.lat;
-                             this.pickerLng = position.lng;
-                        }
-                        this.reverseGeocode({lat: this.pickerLat, lng: this.pickerLng});
+                        this.pickerLat = event.latLng.lat();
+                        this.pickerLng = event.latLng.lng();
+                        this.reverseGeocode(event.latLng);
                     });
 
                     this.pickerMap.addListener("dragstart", () => {
                         this.isMapDragging = true;
                         this.pickerAddress = "Đang di chuyển...";
                     });
+
+                    this.pickerMap.addListener("idle", () => {
+                        const center = this.pickerMap.getCenter();
+                        this.pickerLat = center.lat();
+                        this.pickerLng = center.lng();
+                        // Update marker position
+                        this.pickerMarker.setPosition(center);
+                        this.reverseGeocode(center);
+                    });
+                },
 
                 // Reverse geocode
                 reverseGeocode(latlng) {
