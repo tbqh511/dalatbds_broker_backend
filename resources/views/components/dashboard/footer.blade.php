cf196c6539c874e76284c82e7bf344218d43d910
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2026-01-22 10:36:56 +0700

    Update footer.blade.php

--- resources/views/components/dashboard/footer.blade.php
+++ resources/views/components/dashboard/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="dashboard-footer">
     <div class="dashboard-footer-links fl-wrap">
-        <a href="{{ route('webapp.add_listing') }}" class="footer-btn color-bg">Thêm BĐS</a>
-        <a href="#" class="footer-btn color-bg">Thêm Khách</a>
+        <a href="{{ route('webapp.add_listing') }}" class="btn color-bg">Thêm BĐS</a>
+        <a href="#" class="btn color-bg">Thêm Khách</a>
     </div>
     <a href="#main" class="dashbord-totop custom-scroll-link"><i class="fas fa-caret-up"></i></a>
 </div>
