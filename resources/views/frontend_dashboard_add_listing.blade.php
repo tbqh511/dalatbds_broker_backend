5969a1fc844a90c3b1f94172a0694054eb575b66
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2026-01-31 16:26:23 +0700

    Update frontend_dashboard_add_listing.blade.php

--- resources/views/frontend_dashboard_add_listing.blade.php
+++ resources/views/frontend_dashboard_add_listing.blade.php
@@ -389,7 +389,9 @@
                 pickerLng: null,
                 isMapDragging: false,
                 pickerMarker: null,
-                mapType: 'hybrid',
+                mapType: 'roadmap',
+                //mapType: 'hybrid',
+               
 
                 toggleMapType() {
                     if (!this.pickerMap) return;
